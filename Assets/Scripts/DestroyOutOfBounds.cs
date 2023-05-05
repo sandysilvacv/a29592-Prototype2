e06c51d05763e8be2f0eeee5a9aed96da2c3a538
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-05 12:17:55 +0100

    Acionar uma mensagem de “Game Over”
    Fim do jogo” se algum animal passar pelo jogador.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -10,18 +10,21 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { // Se um objeto passar da visão do jogo, remova esse objeto
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
 
-        } else if (transform.position.z < lowerBound)
+        }
+        else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over");
+
             Destroy(gameObject);
         }
     }
