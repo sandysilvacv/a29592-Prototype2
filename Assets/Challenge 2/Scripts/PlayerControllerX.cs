a8c1d1c31bac875ac3acb76b984013aebf25c1a6
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-05 19:45:56 +0100

    9. Bônus: O jogador pode “spam” a tecla da barra de espaço
    Só permita que o jogador gere um novo cão após um certo período de tempo.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float cooldow = 0;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && cooldow <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldow = 1;
+        }
+
+        if (cooldow >= 0)
+        {
+            cooldow -= Time.deltaTime;
         }
     }
 }
