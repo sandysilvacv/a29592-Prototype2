588139f75d55a3290a7d1a9db3d20566d3471e78
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-05 19:15:24 +0100

    6. Nada está sendo destruído fora da tela

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,12 +16,12 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     { // Se um objeto passar da visão do jogo, remova esse objeto
-        if (transform.position.z > topBound)
+        if (transform.position.z < topBound)
         {
             Destroy(gameObject);
 
         }
-        else if (transform.position.z < lowerBound)
+        else if (transform.position.y < lowerBound)
         {
             Debug.Log("Game Over");
 
